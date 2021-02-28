75085bf556e2df557ae52056c346c930e85a16f5
Bá Lương <38003681+anhsuplo-87@users.noreply.github.com>
2021-02-28 23:58:10 +0700

    Create lesson03.cpp

---
+++ SDL2-tutorials/lesson03/lesson03.cpp
@@ -0,0 +1,144 @@
+//Using SDL and iostream
+#include <SDL.h>
+#include <iostream>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gXOut = NULL;
+
+int main(int argc, char* args[])
+{
+    //Start up SDL and create window
+    if (!init())
+    {
+        std::cout << "Falled to initialize!\n";
+    }
+    else
+    {
+
+        //Load media
+        if (!loadMedia())
+        {
+            std::cout << "Failed to load media\n";
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
+
+            //Event handler
+            SDL_Event e;
+
+            //While application is running
+            while (!quit)
+            {
+                //Handle events on queue
+                while (SDL_PollEvent(&e) != 0)
+                {
+                    //User requests quit
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+
+                //Apply the image
+                SDL_BlitSurface(gXOut,
+                                NULL,
+                                gScreenSurface,
+                                NULL);
+
+                //Update the surface
+                SDL_UpdateWindowSurface(gWindow);
+            }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError();
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow("SDL Tutorial",
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED,
+                                   SCREEN_WIDTH,
+                                   SCREEN_HEIGHT,
+                                   SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            std::cout << "Window could not be created! SDL_Error: " << SDL_GetError();
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gXOut = SDL_LoadBMP("x.bmp");
+    if (gXOut == NULL)
+    {
+        std::cout << "Unable to load image " << "x.bmp" << "! SDL Error: " << SDL_GetError();
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface(gXOut);
+    gXOut = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    //Quit SDL Subsystems
+    SDL_Quit();
+}
